7f240465eaf7fef02d1847f3c31e4be957d7cf80
jahm1997 <jahm1997@gmail.com>
2023-04-19 15:57:10 -0500

    correción update boss

--- src/controllers/Bosses/updateBoss.js
+++ src/controllers/Bosses/updateBoss.js
@@ -6,24 +6,27 @@ const bcrypt = require("bcrypt");
 
 const updateBoss = async (data, path) => {
   console.log("ESTO ES UPDATE-BOSS", data);
-  const { password } = data;
   if (path) {
     const img = fs.readFileSync(path).buffer;
     const logo = await uploadFile(img, "boss");
     const dataAct = { ...data, logo };
     var id = dataAct.id;
     delete dataAct.id;
-    dataAct.password= bcrypt.hashSync(password, 10)
+    if (data["password"]) {
+      dataAct.password = bcrypt.hashSync(password, 10);
+    }
     var [resultado] = await Boss.update(dataAct, {
       where: {
-        id,        
+        id,
       },
     });
   } else {
     const dataAct = { ...data };
     var id = dataAct.id;
     delete dataAct.id;
-    dataAct.password= bcrypt.hashSync(password, 10)
+    if (data["password"]) {
+      dataAct.password = bcrypt.hashSync(password, 10);
+    }
     var resultado = await Boss.update(dataAct, {
       where: {
         id,
